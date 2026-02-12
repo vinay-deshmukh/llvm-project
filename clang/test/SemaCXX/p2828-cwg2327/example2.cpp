0470ebaa646cc46cd5b7b93b3ac89972f6f4ba5b
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2026-02-12 16:24:13 +0530

    example2 is passing

--- clang/test/SemaCXX/p2828-cwg2327/example2.cpp
+++ clang/test/SemaCXX/p2828-cwg2327/example2.cpp
@@ -1,19 +1,21 @@
 
-// RUN: %clang_cc1 %s -std=c++14 -fsyntax-only -verify=since-cxx14 -ast-dump 2>&1 | FileCheck -allow-empty %s 
+// RUN: %clang_cc1 %s -std=c++14 -fsyntax-only -verify=since-cxx14 -ast-dump 2>&1 
 // RUN: %clang_cc1 %s -std=c++17 -fsyntax-only -ast-dump 2>&1 | FileCheck -check-prefix=CHECK-CXX17 %s
 
 // expected-no-diagnostics
 
 // Example 2
-struct X {
-X(int);
+// since-cxx14-note@+2{{candidate constructor (the implicit move constructor)}}
+// since-cxx14-note@+1{{candidate constructor (the implicit copy constructor)}}
+struct X {  
+X(int); //since-cxx14-note {{candidate constructor}}
 // X(X&&); // implicitly declared
 };
 struct Y {
 operator X();
 operator int();
 };
-X x(Y{}); // since-cxx14-error {{call to constructor of 'X' is ambigious}}
+X x(Y{}); // since-cxx14-error {{call to constructor of 'X' is ambiguous}}
 
 /*
 CHECK-CXX17: `-VarDecl {{.*}} x 'X' callinit
