40bf2ec179fc95ee0645a8652e8bb08468270d95
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2026-02-12 16:08:05 +0530

    example2 will fail

--- clang/test/SemaCXX/p2828-cwg2327/example2.cpp
+++ clang/test/SemaCXX/p2828-cwg2327/example2.cpp
@@ -1,5 +1,7 @@
 
-// RUN: %clang_cc1 -fsyntax-only -verify %s -std=c++23
+// RUN: %clang_cc1 %s -std=c++14 -fsyntax-only -verify=since-cxx14 -ast-dump 2>&1 | FileCheck -allow-empty %s 
+// RUN: %clang_cc1 %s -std=c++17 -fsyntax-only -ast-dump 2>&1 | FileCheck -check-prefix=CHECK-CXX17 %s
+
 // expected-no-diagnostics
 
 // Example 2
@@ -11,4 +13,15 @@ struct Y {
 operator X();
 operator int();
 };
-X x(Y{});
+X x(Y{}); // since-cxx14-error {{call to constructor of 'X' is ambigious}}
+
+/*
+CHECK-CXX17: `-VarDecl {{.*}} x 'X' callinit
+CHECK-CXX17-NEXT:  `-ExprWithCleanups {{.*}} 'X'
+CHECK-CXX17-NEXT:    `-ImplicitCastExpr {{.*}} 'X' <UserDefinedConversion>
+CHECK-CXX17-NEXT:      `-CXXMemberCallExpr {{.*}} 'X'
+CHECK-CXX17-NEXT:        `-MemberExpr {{.*}} '<bound member function type>' .operator X {{.*}}
+CHECK-CXX17-NEXT:          `-MaterializeTemporaryExpr {{.*}} 'Y' lvalue
+CHECK-CXX17-NEXT:            `-CXXFunctionalCastExpr {{.*}} 'Y' functional cast to Y <NoOp>
+CHECK-CXX17-NEXT:              `-InitListExpr {{.*}} 'Y'
+*/
